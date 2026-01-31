c1227968e4cf0c28e1c03f854abacf4110dbaa5e
gurpreets@neuronimbus.com-at-138234575190 <gurpreets@neuronimbus.com>
2026-01-31 11:48:57 +0530

    changes

--- blocks/footer-bottoms/footer-bottoms.js
+++ blocks/footer-bottoms/footer-bottoms.js
@@ -16,14 +16,14 @@ export default function decorate(block) {
 
   // Column 1
   const col1 = document.createElement("div");
-  col1.className = "col-md-4 copyright opacity-50";
+  col1.className = "col-md-3 copyright opacity-50";
   col1.append(items[0]);
   wrapper.append(col1);
 
   // Column 2
   const col2 = document.createElement("div");
   col2.className =
-    "col-md-3 d-flex justify-content-center align-items-center logo fw-medium gap-3";
+    "col-md-3 d-flex justify-content-end align-items-center logo fw-medium gap-3";
   const span = document.createElement("span");
   span.textContent = "SECURED BY:";
   col2.append(span);
@@ -33,7 +33,7 @@ export default function decorate(block) {
 
   // Column 3
   const col3 = document.createElement("div");
-  col3.className = "col-md-5 links";
+  col3.className = "col-md-6 links";
   col3.append(items[2]);
   wrapper.append(col3);
 
