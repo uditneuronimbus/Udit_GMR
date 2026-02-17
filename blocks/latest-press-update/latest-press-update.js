b408a98c30ab3786660d76281347dbfe074fad19
gurpreets@neuronimbus.com-at-138234575190 <gurpreets@neuronimbus.com>
2026-02-17 16:59:05 +0530

    changes

--- blocks/latest-press-update/latest-press-update.js
+++ blocks/latest-press-update/latest-press-update.js
@@ -37,7 +37,7 @@ export default async function decorate(block) {
   const offset = 0;
   const category = getCategoryFromURL();
   let labeltitle = '';
-  if (category === "press-release") {
+  if (category === "press-releases") {
     labeltitle = "LATEST PRESS RELEASE";
   } else if (category === "blog") {
     labeltitle = "LATEST INSIGHTS";
