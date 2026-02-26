8e9aabb88800f330b94308cf3d3432236877623a
gurpreets@neuronimbus.com-at-138234575190 <gurpreets@neuronimbus.com>
2026-02-26 16:43:24 +0530

    changes

--- blocks/success-stories/success-stories.js
+++ blocks/success-stories/success-stories.js
@@ -94,7 +94,7 @@ export default async function decorate(block) {
       const link = item?.slugUrl || "#";
 
       slide.innerHTML = `
-        <div class="card card-ui h-100 p-4 asdf">
+        <div class="card card-ui h-100 p-4">
           <div class="card-img">
             <a href="story-update?post=${link}">
               <img
@@ -104,7 +104,9 @@ export default async function decorate(block) {
             </a>
           </div>
           <div class="card-body">
+            <h5 class="card-title">
             <a href="story-update?post=${link}">${item.title || ""}</a>
+            </h5>
             <p class="card-text">${item.description?.plaintext || ""}</p>
             <a href="story-update?post=${link}" class="btn-link">
               ${item.ctaText?.plaintext || "READ MORE"}
